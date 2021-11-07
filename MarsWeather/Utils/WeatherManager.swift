e62062d9403c90f305d504954ce0335cddc4cde9
hvdub24 <havenvw@gmail.com>
2021-11-07 00:43:27 -0400

    API finishing

--- MarsWeather/Utils/WeatherManager.swift
+++ MarsWeather/Utils/WeatherManager.swift
@@ -4,20 +4,24 @@
 //
 //  Created by Haven Van Wormer on 11/6/21.
 //
-/*
+
 import Foundation
 
-class WeatherManager: NSObject, ObservableObject {
+
+
+class WeatherManager: ObservableObject {
     @Published private(set) var weather = Weather()
     private let apiManager = APIManager()
     
+  
     
     func updateWeatherInfo() {
-        apiManager.getWeather() { weather in
-            self.weather = weather
-        }
+      
+            apiManager.getWeather() { weather in
+                self.weather = weather
+            }
         
     }
     
 }
- */
+
