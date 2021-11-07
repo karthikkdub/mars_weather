273717b1994bcbe19abfaeb322e9b3e94328adb9
hvdub24 <havenvw@gmail.com>
2021-11-06 21:41:35 -0400

    Some UI Changes

--- MarsWeather/ContentView.swift
+++ MarsWeather/ContentView.swift
@@ -12,8 +12,19 @@ struct ContentView: View {
    
     var body: some View {
         
-            
+        VStack(alignment: .leading){
+            Text("Curiosity Rover").font(.system(size: 36, weight: .semibold))
+            Text("Gale Crater").font(.system(size: 25)).italic()
+        }.foregroundColor(.white)
+        
+        .background(
+            Image("marsImg")
+        )
     }
+    
+    
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
